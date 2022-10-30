caba68ab0456b51f6035db4ed9a98cd0607bddee
masterroomdevelopers <masterroomdevelopers@gmail.com>
2022-10-30 11:05:37 +0530

    added code for stripe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -29,7 +31,7 @@ export class CartComponent implements OnInit {
     'action',
   ];
 
-  constructor(private _cartService: CartService) {}
+  constructor(private _cartService: CartService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this._cartService.cart.subscribe((cart: Cart) => {
@@ -56,5 +58,21 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem){
     this._cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe({
+      next: async (data: any) => {
+        let stripe = await loadStripe("pk_test_51LyTBDSILt1YPv2QR8p4tTQbCJCqVXQmWJgdJlmsoulixUzrN7QGbXlm3s6Ajoy4wej5A4Ifg2I8wyOsi2jbAGpu00U6LPEVYC");
+        stripe?.redirectToCheckout({
+          sessionId: data.id
+        })
+      },
+      error: (err) => {
+
+      }
+    })
   }
 }
